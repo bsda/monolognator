fa046eed1a32c278dda7241fe87443563141ff8b
Jambock <jambock@users.noreply.github.com>
2018-07-10 17:25:37 +0100

    Tidy up

--- bot.py
+++ bot.py
@@ -5,17 +5,18 @@ import logging
 import os
 import re
 
-logging.basicConfig(level=logging.INFO,
+logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 counter = {}
 previous_user = None
 msg_limit = random.randint(5, 12)
+my_chat_id = 113426151
 print(f'First msg limit: {msg_limit}')
 
-
-images = ['hand', 'bla', 'incoming', 'duck', 'typing1', 'tsunami']
+# List of existing gifs to send to chat
+gifs = ['hand', 'bla', 'incoming', 'duck', 'typing1', 'tsunami']
 
 
 def start(bot, update):
@@ -33,7 +34,6 @@ def set_limit(bot, update):
     global msg_limit
     msg = update.message.text
     msg_limit = int(re.findall('[0-9]+', msg)[0])
-
     logger.info(f'New Limit set by {update.message.from_user.first_name}: {msg_limit}')
 
 
@@ -49,17 +49,20 @@ def count(bot, update):
         counter[user] = {}
         counter[user]['count'] = 1
         counter[user]['msg_ids'] = list()
+        # Add first msg id to counter
         counter[user]['msg_ids'].append(update.message.message_id)
         counter[user]['msgs'] = list()
+        # Add first message to counter
         counter[user]['msgs'].append(update.message.text)
     else:
-        # if current msg is by the same user as previous msg, increase counter
+        # if current msg is by the same user as previous msg,
+        # increase counter, add msg_id and message to counter
         if user == previous_user:
             counter[user]['count'] += 1
             counter[user]['msg_ids'].append(update.message.message_id)
             counter[user]['msgs'].append(update.message.text)
         else:
-            # otherwise, reset counter for previous user
+            # If it's a new user, reset counter for previous user
             print(f'Reseting the counter for {previous_user}')
             counter[previous_user]['count'] = 1
             counter[previous_user]['msg_ids'] = list()
@@ -71,54 +74,46 @@ def count(bot, update):
     print(f'limit: {msg_limit}')
     print(counter[user]['msg_ids'])
     print(counter[user]['msgs'])
-
-    # of count for user = 5, send alert
-    # if counter[user]['count'] == 5:
-    #     bot.send_message(chat_id=113426151, text=f'New msg limit set: {msg_limit}')
-    #     print(f'{user} hit the warning counter')
-        # bot.send_message(chat_id=update.message.chat_id,
-        #                 text=f'Hold your horses {update.message.from_user.first_name},'
-        #                       f' that was a {counter[user]["count"]} line monologue!!!!\n'
-        #                       f'The next time, I will terminate your monologue!')
-
-        # If count = 10, delete previous msgs and send summary
     user_count = counter[user]['count']
+
     if user_count == msg_limit:
         # Clear counter for user
-
         counter[user]['count'] = 0
-        # Reset randon limit
+
+        # Reset random limit
         msg_limit = random.randint(5, 12)
         print(f'New random limit: {msg_limit}')
-        bot.send_message(chat_id=113426151, text=f'New msg limit set: {msg_limit}')
+        bot.send_message(chat_id=my_chat_id, text=f'New msg limit set: {msg_limit}')
 
-        print(f'{user} hit the fuck face counter')
-        #bot.send_message(chat_id=update.message.chat_id,
-        #                 text=f'I told you, fuck face! Im terminating your monologue,'
-        #                      f' {update.message.from_user.first_name}')
+        # Delete messages from group
         for m in set(counter[user]['msg_ids']):
             bot.delete_message(chat_id=update.message.chat_id, message_id=m)
 
-        # Send message with the last 10 message by the user
+        # Send funny gif
         bot.send_document(chat_id=update.message.chat_id,
-                          document=open('./gifs/' + random.choice(images) + '.gif', 'rb'), timeout=100)
+                          document=open('./gifs/' + random.choice(gifs) + '.gif', 'rb'), timeout=100)
+
+        # Send monologue back as a single message
         bot.send_message(chat_id=update.message.chat_id,
                          text='*Monologue by {}*:\n\n`{}`'.format(
                              update.message.from_user.first_name,
                              "\n".join(counter[user]['msgs'])), parse_mode=telegram.ParseMode.MARKDOWN)
 
+        # reset msgs and counters for user
         counter[user]['msgs'] = list()
         counter[user]['msg_ids'] = list()
 
 
+def main():
+
+    updater = Updater(os.getenv('telegram_token'))
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CommandHandler('limit', limit))
+    updater.dispatcher.add_handler(CommandHandler('set_limit', set_limit))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, count))
+    updater.start_polling()
+    updater.idle()
 
 
-updater = Updater(os.getenv('telegram_token'))
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('limit', limit))
-updater.dispatcher.add_handler(CommandHandler('set_limit', set_limit))
-
-
-updater.dispatcher.add_handler(MessageHandler(Filters.text, count))
-updater.start_polling()
-updater.idle()
+if __name__ == '__main__':
+    main()

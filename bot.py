78cbca5e02177f37aecd473cce30c34e170ddf44
Bruno <bsda@users.noreply.github.com>
2020-01-03 12:58:39 +0000

    change print to logging

--- bot.py
+++ bot.py
@@ -30,9 +30,9 @@ gif_path = './gifs/'
 
 
 with open('config.json') as config_file:
-    print('Loading config file...')
+    logger.info('Loading config file...')
     cfg = json.load(config_file)['config']
-    print(f'Loaded config:\n{cfg}')
+    logger.info(f'Loaded config:\n{cfg}')
 
 
 def start(bot, update):

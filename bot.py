5e557c6249e7d3695c84250ffb98e871205fe86a
Bruno <bsda@users.noreply.github.com>
2020-01-09 22:59:43 +0000

    missing import

--- bot.py
+++ bot.py
@@ -19,6 +19,7 @@ from gif import get_random_giphy, search_tenor, inlinequery, informer, lula, slo
 from monologue import query_limit, set_limit, handle_counter
 from weather import get_weather, chance_of_rain_today, chuva, chuva2, scheduled_weather, send_weather
 import twitter
+import string
 
 cfg = config.cfg()
 

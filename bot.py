a1e8b805666950300efdfd4c26e4a1e16403bc2c
Bruno <bsda@users.noreply.github.com>
2020-02-01 13:41:35 +0000

    fix commnand

--- bot.py
+++ bot.py
@@ -257,7 +257,7 @@ def main():
     updater.dispatcher.add_handler(CommandHandler('beer2', beer_search_menu))
     updater.dispatcher.add_handler(CommandHandler('dry', dry_score_message))
     updater.dispatcher.add_handler(CommandHandler('wet', wet_score_message))
-    updater.dispatcher.add_handler(CommandHandler('corona', get_corona()))
+    updater.dispatcher.add_handler(CommandHandler('corona', get_corona))
     updater.dispatcher.add_handler(InlineQueryHandler(inlinequery))
     word_watcher_regex = re.compile('.*(lula|informer|slough|vai ficar tudo bem|calma cara|999London).*', re.IGNORECASE)
     updater.dispatcher.add_handler(RegexHandler(word_watcher_regex, word_watcher))
